1dd29d38c0d3e821e5a45b0d10ccb419f42c4c9d
Blake Latchford <blake.latchford@gmail.com>
2020-04-13 12:59:57 -0400

    Backup configurations in home.

--- home/.config/fish/functions/backup-system.fish
+++
@@ -1,9 +0,0 @@
-function backup-system
-    pushd $system_setup_directory
-        dpkg --get-selections > $system_setup_directory/selections.list
-        cp -vr /etc/apt/sources.list* etc/apt
-        cp -v /etc/default/grub etc/default/grub
-
-        git commit selections.list etc/
-    popd
-end
